42b7625367c96dcd1f1c99c31ed2e81386bcd313
Guilherme <gemdias@hotmail.com>
2021-10-06 22:54:07 -0300

    Melhorando frontend

--- cardapio/cardapio/settings.py
+++ cardapio/cardapio/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'cardapio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dafjhdg5bij83i',
+        'USER':'ijuaregbuyjeox',
+        'PASSWORD':'ac716ca517161861b37976c522fdcb56c1badaa5fb888bf6bcfe407b9045320f',
+        'HOST':'ec2-52-23-87-65.compute-1.amazonaws.com',
+        'PORT':'5432'
+    }
+}
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'dafjhdg5bij83i',
-#        'USER':'ijuaregbuyjeox',
-#        'PASSWORD':'ac716ca517161861b37976c522fdcb56c1badaa5fb888bf6bcfe407b9045320f',
-#        'HOST':'ec2-52-23-87-65.compute-1.amazonaws.com',
-#        'PORT':'5432'
-#    }
-#}
-
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
