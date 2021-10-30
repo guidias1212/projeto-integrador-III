97ff04ccdaf7d1c387add49fb9120d6d64a552ac
Guilherme <gemdias@hotmail.com>
2021-10-29 23:34:45 -0300

    Configurando id autoincrementavel

--- cardapio/cardapio_virtual/models.py
+++ cardapio/cardapio_virtual/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    id = models.CharField(max_length=20, primary_key=True)
     nome = models.CharField(max_length=20)
     descricao = models.CharField(max_length=20)
     imagem = models.CharField(max_length=256)
