6e2089ffa4f294618efe283dc8ad3aecc3e5a971
Guilherme <gemdias@hotmail.com>
2021-10-30 00:15:31 -0300

    Ajustando limitacoes de campos

--- cardapio/cardapio_virtual/models.py
+++ cardapio/cardapio_virtual/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    nome = models.CharField(max_length=20)
-    descricao = models.CharField(max_length=20)
-    imagem = models.CharField(max_length=256)
-    preco = models.CharField(max_length=20)
+    nome = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=200)
+    imagem = models.CharField(max_length=512)
+    preco = models.DecimalField(max_digits=9, decimal_places=2)
+
     class Meta:
         db_table = "itens"
