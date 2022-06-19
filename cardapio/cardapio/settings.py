e7febeadc5295f5d2d4884c211414f5545ec81fc
limadmd <limadmd@gmail.com>
2022-06-19 16:05:39 -0300

    adição do vlibras para demais telas do projeto

--- cardapio/cardapio/settings.py
+++ cardapio/cardapio/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'cardapio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -99,7 +99,7 @@ DATABASES = {
         'PORT': os.environ['DATABASE_PORT']
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
